b29fc44b8981da5456e257fa5e49f0883b565fdc
Luis Enrique <enriquecalero284@gmail.com>
2024-08-27 15:40:39 -0500

    SliderScroll acabado ,solo puliu

--- src/utils/CustomEventListener.ts
+++ src/utils/CustomEventListener.ts
@@ -1,50 +1,78 @@
+type Direction = "LEFT" | "RIGTH";
+
 export const addCustomEventListener = (
-    typeEvent: keyof DocumentEventMap,
-    selector: string,
-    callback: (e: Event) => void,
-    parent: HTMLElement | Document = document
-  ) => {
-    parent.addEventListener(typeEvent, (e) => {
-      const target = e.target as HTMLElement | null;
-      if (target && target.matches(selector)) {
-        callback(e);
-      }
-    });
-  };
-  
-  
-  
-  export const getSiblings = (elem: HTMLElement, parent?: string | HTMLElement) => {
-    const parentElement =
-      typeof parent === "string"
-        ? elem.closest(parent).children
-        : elem.parentNode.children;
-    const $Hermanos = parentElement;
-    return Array.from($Hermanos).filter(
-      (hermano) => hermano !== elem
-    ) as HTMLElement[];
-  };
-  
-  export const removeClassElements = (
-    elements: HTMLElement[],
-    selectorClass: string
-  ) => {
-    elements.forEach((element) => element.classList.remove(selectorClass));
-  };
-  
-  export const findPreviousSiblingWithClass = (
-    element: HTMLElement,
-    className: string
-  ) => {
-    let sibling = element.previousElementSibling;
-  
-    while (sibling) {
-      if (sibling.classList.contains(className)) {
-        return sibling as HTMLElement;
-      }
-      sibling = sibling.previousElementSibling;
+  typeEvent: keyof DocumentEventMap,
+  selector: string,
+  callback: (e: Event) => void,
+  parent: HTMLElement | Document = document
+) => {
+  parent.addEventListener(typeEvent, (e) => {
+    const target = e.target as HTMLElement | null;
+    if (target && target.matches(selector)) {
+      callback(e);
     }
-  
-    return null;
-  };
-  
+  });
+};
+
+export const getSiblings = (
+  elem: HTMLElement,
+  parent?: string | HTMLElement
+) => {
+  const parentElement =
+    typeof parent === "string"
+      ? elem.closest(parent).children
+      : elem.parentNode.children;
+  const $Hermanos = parentElement;
+  return Array.from($Hermanos).filter(
+    (hermano) => hermano !== elem
+  ) as HTMLElement[];
+};
+
+export const removeClassElements = (
+  elements: HTMLElement[],
+  selectorClass: string
+) => {
+  elements.forEach((element) => element.classList.remove(selectorClass));
+};
+
+export const findPreviousSiblingWithClass = (
+  element: HTMLElement,
+  className: string
+) => {
+  let sibling = element.previousElementSibling;
+
+  while (sibling) {
+    if (sibling.classList.contains(className)) {
+      return sibling as HTMLElement;
+    }
+    sibling = sibling.previousElementSibling;
+  }
+
+  return null;
+};
+export const obtenerHermanos = (
+  elementoInicial: HTMLElement,
+  direction: Direction,
+  cantidad: number
+): { ultimoHermano: HTMLElement | null; hermanos: HTMLElement[] } => {
+  const hermanos: HTMLElement[] = [];
+  let elemento = elementoInicial;
+
+  if (direction === "RIGTH") {
+    while (elemento.nextElementSibling && hermanos.length < cantidad) {
+      elemento = elemento.nextElementSibling as HTMLElement;
+      hermanos.push(elemento);
+    }
+  } else {
+    // Si no es "RIGHT", asumimos que es "LEFT"
+    while (elemento.previousElementSibling && hermanos.length < cantidad) {
+      elemento = elemento.previousElementSibling as HTMLElement;
+      hermanos.push(elemento);
+    }
+  }
+
+  const ultimoHermano =
+    hermanos.length > 0 ? hermanos[hermanos.length - 1] : null;
+
+  return { ultimoHermano, hermanos };
+};
